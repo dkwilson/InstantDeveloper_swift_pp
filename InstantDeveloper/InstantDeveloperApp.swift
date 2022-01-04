3748182245c5eeb990195f6d4ef4dd3ba4953708
dkwilson <general@ytechnology.net>
2022-01-03 23:28:32 -0500

    Initial Commit

---
+++ InstantDeveloper/InstantDeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantDeveloperApp.swift
+//  InstantDeveloper
+//
+//  Created by Dkeith Wilson on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct InstantDeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
